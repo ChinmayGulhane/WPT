8ba832ac405aff46cc5f362b234dee3208471c49
Chinmay Gulhane <71486696+ChinmayGulhane@users.noreply.github.com>
2023-12-13 08:13:15 +0530

    Add files via upload

---
+++ Day4/code/javascript/funcex11.js
@@ -0,0 +1,36 @@
+//pass function as a parameter
+//to another function
+
+function doJob(f1)
+{
+    for(var i=0;i<5;i++)
+    {
+        f1();
+    }
+}
+
+
+doJob(function f1() {
+    console.log("hi")
+})
+
+doJob(()=>{console.log("heys")})
+
+console.log("--------")
+test()
+console.log("--------")
+
+//named function
+function test()
+{
+    console.log("test")
+}
+
+doJob(test)
+
+let v = function()
+{
+    console.log("bye")
+}
+
+doJob(v)
